67325a37ae3611c43de05bc745efda2462a4019a
cretivoxdev <95409631+cretivoxdev@users.noreply.github.com>
2022-08-09 13:10:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,16 @@ def index():
     return render_template('index.html')
 @app.route('/kol',  methods = ["POST","GET"])
 def kol():
+    user = " "
     usrname = str(request.form.get("usrname"))
     print(usrname)
-    session_id = "51669758945%3AQXe1CxsKaeEl0Z%3A14%3AAYcitzOLUpfaaC582rAfRbhWnz_5QSUIPnSNdhQYOQ"
+    # session_ig = ["48420109999%3AwYTofPNHOHFmPo%3A15%3AAYdePiWOn0bg3xhjxthCXGZ4ogwQzl2hbuT-QFLjbw" , "51669758945%3AQXe1CxsKaeEl0Z%3A14%3AAYcitzOLUpfaaC582rAfRbhWnz_5QSUIPnSNdhQYOQ"]
+    session_id = "51669758945%3AeaoE7wCFnfMUUz%3A20%3AAYeauCt70M82lH0aKr9c-h3ZnUmHS5lgY6fKsNQGyw"
     try:
         user = InstagramUser(usrname, sessionid=session_id)
+        print("user : ", user)
     except:
+        print("user : ", user)
         return render_template("index.html")
     user = InstagramUser(usrname, sessionid=session_id)
     ig_user = user.fullname
